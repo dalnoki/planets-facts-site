4edcb58002320b699df5daa178dcf13e2021fe1e
ZsPapp <zsofia.papp@sentry.io>
2024-03-16 11:22:15 +0100

    fix mobile menu

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -2,10 +2,21 @@ import "../main.scss";
 import hamburger from "../assets/icon-hamburger.svg";
 import { Link } from "react-router-dom";
 import planetData from "../../data.json";
+import { useState } from "react";
+import clsx from "clsx";
 
 const allPlanets = planetData;
 
 export default function Nav() {
+  const [isMenuToggled, setIsMenuToggled] = useState(false);
+
+  const handleToggle: () => void = () => setIsMenuToggled(!isMenuToggled);
+
+  const hamburgerMenuStyle = clsx({
+    ["nav-planets"]: true,
+    ["menu-toggled"]: isMenuToggled,
+  });
+
   return (
     <nav className="nav">
       <ul>
@@ -19,10 +30,11 @@ export default function Nav() {
           src={hamburger}
           width={24}
           height={17}
+          onClick={handleToggle}
         ></img>
       </label>
-      <input id="menu" type="checkbox" />
-      <ul className="nav-planets">
+      <input className="nav-checkbox" id="menu" type="checkbox" />
+      <ul className={hamburgerMenuStyle}>
         {allPlanets.map((planet) => (
           <div className="nav-planets-container">
             <span
